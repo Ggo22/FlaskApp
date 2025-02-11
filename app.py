c0ffe40ff300c9c4dccefd465b63b1697cbffb0f
ALLAN <ngallemoudjongue@gmail.com>
2025-02-11 21:06:37 +0100

    Initial commit - Flask appand tests

---
+++ app.py
@@ -0,0 +1,44 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1dc33d14-4f9c-4615-8737-450919549625",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from flask import Flask\n",
+    "\n",
+    "app=Flask(__name__)\n",
+    "\n",
+    "@app.route('/')\n",
+    "def hello_world():\n",
+    "    return 'Hello, World!'\n",
+    "\n",
+    "if __name__=='__main__' :\n",
+    "    app.run(host='0.0.0.0',port=5000)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ test_app.py
@@ -0,0 +1,42 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d1ac3a9d-22e4-4558-9558-5cffc689ce45",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import pytest\n",
+    "from app import app\n",
+    "\n",
+    "def test_index():\n",
+    "    tester=app.test_client()\n",
+    "    reponse=tester.get('/')\n",
+    "    assert response.status_code==200\n",
+    "    assert b\"Hello,World!\" in response.data"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
